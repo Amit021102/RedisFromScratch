b0302ee3ade5d248aaa1a68e55cac0428571a983
Amit Leshem <amitleshem10@gmail.com>
2026-08-26 16:11:01 +0300

    basic structure

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Listening on port :6379")
+
+	// create a new server
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+
+	// listen for connections
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection:", err)
+		return
+	}
+
+	defer conn.Close() // close connection once finished
+
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expected array")
+			continue
+		}
+
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, expected array length > 0")
+			continue
+		}
+
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		// fmt.Println("Received command:", command, "with args:", args)
+
+		writer := NewWriter(conn)
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		result := handler(args)
+		writer.Write(result)
+	}
+}
